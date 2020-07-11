393daa45f187deedb56e040d899eae2879b8b67a
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-07-11 14:00:24 -0500

    ejercicio 5 solved

---
+++ ejercicio5.js
@@ -0,0 +1,36 @@
+const peek = (stack) => {
+    return stack[stack.length - 1]
+  }
+  
+const isBalanced = (str) => {
+    let opening = '(['
+    let closing = ')]'
+    let stack = []
+    for(let i =0; i < str.length; i++){
+      let char = str.charAt(i)
+      if(opening.includes(char)){
+          stack.push(char)
+      } else if(closing.includes(char)){
+          if(stack.length === 0){
+              return false
+          } else {
+              let first = peek(stack)
+              if(opening.indexOf(first) === closing.indexOf(char)){
+                  stack.pop()
+              } else {
+                  return false
+              }
+          }
+      }
+    }
+    return stack.length === 0
+  }
+
+  console.log(isBalanced('([])'))
+
+  console.log(isBalanced('[()[]]'))
+
+  console.log(isBalanced('([)}'))
+  console.log(isBalanced('([)}'))
+  console.log(isBalanced('([)}'))
+  console.log(isBalanced('()(([)}'))
